09556f9756b1e848496a576e5afba2779f44da1e
Grattyelle Teles <grattyelleteles@gmail.com>
2020-12-06 23:47:31 -0300

    Criada uma app "base"

--- utilrh/urls.py
+++ utilrh/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from utilrh.base.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', home),
+
+
 ]
